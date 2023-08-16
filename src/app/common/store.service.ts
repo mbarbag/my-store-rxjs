e9920b552fda3a49f40821fca7aca0919a61af5c
Manuela Barba <mbarbag@unal.edu.co>
2023-08-16 13:18:14 -0500

    adding logic for load the backend not from home-component but as the app is initialized

--- src/app/common/store.service.ts
+++ src/app/common/store.service.ts
@@ -1,11 +1,43 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable, Subject } from "rxjs";
+import { BehaviorSubject, Observable, Subject, timer } from "rxjs";
 import { Course } from "../model/course";
+import { createHttpObservable } from "./util";
+import { delayWhen, map, retryWhen, shareReplay, tap } from "rxjs/operators";
 
 @Injectable({
     providedIn: 'root'
 })
 export class Store {
+        
     private subject = new BehaviorSubject<Course[]>([]);
     courses$: Observable<Course[]> = this.subject.asObservable();
+
+    init() {
+        const http$ = createHttpObservable('/api/courses');
+
+        http$
+            .pipe(
+                tap(() => console.log("HTTP request executed")),
+                map(res => Object.values(res["payload"])),
+            )
+            .subscribe(
+                courses => this.subject.next(courses),
+            );
+    }
+
+    selectBeginnerCourses(): Observable<Course[]> {
+        return this.filterByCategory('BEGINNER');
+    }
+
+    selectAdvancedCourses(): Observable<Course[]> {
+        return this.filterByCategory('ADVANCED');
+    }
+
+    filterByCategory(category: string) {
+        return this.courses$
+        .pipe(
+            map(courses => courses
+                .filter(course => course.category == category))
+        );
+    }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,31 +23,11 @@ export class HomeComponent implements OnInit {
 
     ngOnInit() {
 
-        const http$ = createHttpObservable('/api/courses');
+        const courses$ = this.store.courses$;
 
-        const courses$: Observable<Course[]> = http$
-            .pipe(
-                tap(() => console.log("HTTP request executed")),
-                map(res => Object.values(res["payload"]) ),
-                shareReplay(),
-                retryWhen(errors =>
-                    errors.pipe(
-                    delayWhen(() => timer(2000)
-                    )
-                ) )
-            );
+        this.beginnerCourses$ = this.store.selectBeginnerCourses();
 
-        this.beginnerCourses$ = courses$
-            .pipe(
-                map(courses => courses
-                    .filter(course => course.category == 'BEGINNER'))
-            );
-
-        this.advancedCourses$ = courses$
-            .pipe(
-                map(courses => courses
-                    .filter(course => course.category == 'ADVANCED'))
-            );
+        this.advancedCourses$ = this.store.selectAdvancedCourses();
 
     }
 
